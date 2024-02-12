c4670026147ac1dd83e50a7ef6f78b9e53b73dd9
Damien Ostler <damien.ostler@data443.com>
2024-02-11 20:53:36 -0500

    change name

--- components/header.tsx
+++ components/header.tsx
@@ -66,7 +66,7 @@ function ResponsiveAppBar() {
               textDecoration: 'none',
             }}
           >
-            COMISSIONS.APP
+            REQUEST.BOX
           </Typography>
                 </Box>
 
