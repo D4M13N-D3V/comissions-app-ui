66b0c7be808c34aed28ae09b2cd885b4d8da1637
Damien Ostler <damien.ostler@data443.com>
2024-02-13 23:25:57 -0500

    removed random string in responses

---
+++ pages/api/seller/profile.tsx
@@ -0,0 +1,15 @@
+import { getAccessToken, withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
+
+export default withApiAuthRequired(async function sellerProfile(req, res) {
+  console.log("TEST")
+  const { accessToken } = await getAccessToken(req, res);
+  const response = await fetch(process.env.NEXT_PUBLIC_API_URL+'/api/SellerProfile', {
+    headers: {
+      "Authorization": `Bearer ${accessToken}`
+    }
+  });
+  
+  let result = await response.json();
+  res.status(200).json(result);
+});
+
